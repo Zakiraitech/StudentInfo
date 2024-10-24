38104894a242435160c4740db40bd30d2eeb34b2
HAMZA ALIYU <haiztech37@gmail.com>
2024-10-25 00:30:03 +0300

    commit #39
    i declared a method

--- app/src/main/java/com/example/studentinfo/StudentAdapter.java
+++ app/src/main/java/com/example/studentinfo/StudentAdapter.java
@@ -1,7 +1,16 @@
 package com.example.studentinfo;
 
+import androidx.annotation.NonNull;
+
 import java.util.List;
 
 public class StudentAdapter {
     private List<Student> studentList;
+
+    public StudentAdapter(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    @NonNull
+    @Override
 }
